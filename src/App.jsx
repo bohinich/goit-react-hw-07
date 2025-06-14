c5bc850e2c57281d3456e2b67cb094da6a58ddf1
Bohinich Serhii <bohinich1@gmail.com>
2025-06-14 17:05:51 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import ContactForm from './components/ContactForm/ContactForm';
+import ContactForm from './components/ContactsForm/ContactsForm';
 import ContactList from './components/ContactList/ContactList';
 import Filter from './components/Filter/Filter';
 import { fetchContacts } from './redux/contactsOps';
